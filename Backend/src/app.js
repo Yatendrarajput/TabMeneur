653893a96df7b2547aa79e9b224c4d0865b49a89
pranjali6 <itspranjali06@gmail.com>
2024-07-07 18:38:11 +0530

    User Registration

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -14,5 +14,10 @@ app.use(express.urlencoded({extended:true,limit:"16kb"}));
 app.use(express.static("public"))
 app.use(cookieParser())
 
+// Routes import
+import userRouter from "./routes/user.routes.js"
+// Routes Declare
+app.use("/api/v1/users",userRouter)
+
 export {app}
 
--- Backend/src/models/users.models.js
+++ Backend/src/models/users.models.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema=new mongoose.Schema({
     username:{
@@ -33,5 +35,43 @@ const userSchema=new mongoose.Schema({
     }]
 
 },{timestamps:true});
+
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
+    this.password=await bcrypt.hash(this.password,10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect=async function(password){
+    return await bcrypt.compare(password,this.password);
+}
+
+userSchema.methods.generateAccessToken=async function(){
+    const accessToken=jwt.sign(
+        {
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+        }
+    )
+    return accessToken
+}
+
+userSchema.methods.generateRefreshToken=async function(){
+    const refreshToken=jwt.sign(
+        {
+        _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+        }
+    )
+    return refreshToken
+}
 
 export const User=mongoose.model("User",userSchema);
